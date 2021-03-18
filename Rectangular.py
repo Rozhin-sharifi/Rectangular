b945e8aabe97ebc75bf863bd307cd1726000fa75
Rozhin-sharifi <80633561+Rozhin-sharifi@users.noreply.github.com>
2021-03-18 18:48:48 +0330

    Add files via upload

---
+++ Rectangular.py
@@ -0,0 +1,8 @@
+a= float (input())
+b= float (input())
+c= float (input())
+
+if a+b<c and a+c<b and b+c<a:
+    print("well done! It is a rectangular")
+else:
+    print("Oop! It is not a rectangular")
